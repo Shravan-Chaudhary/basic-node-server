04216ce9bfe6bf8f2e67b74533531b436e3a8d72
Shravan Chaudhary <shravansinghchaudhary@gmail.com>
2023-05-07 19:28:26 +0530

    basic server with raw node

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 const fs = require('fs')
+const http = require('http')
 
-const text = fs.readFileSync('./text.txt', 'utf-8')
+// Reading files
+// const text = fs.readFileSync('./text.txt', 'utf-8')
 
-const text2 = `This is my the line i writei winter: ${text} \n On: ${Date.now()}`
+// const text2 = `This is my the line i writei winter: ${text} \n On: ${Date.now()}`
 
-fs.writeFileSync('./text2.txt', text2)
+// fs.writeFileSync('./text2.txt', text2)
 
-console.log('Done')
+// console.log('Done')
+
+// Server
+const server = http.createServer((req, res) => {
+  // console.log(req.url, req.headers)
+  const url = req.url
+  if (url === '/') {
+    return res.end('Hii')
+  }
+  req.write('no hii')
+})
+
+server.listen(3000, () => {
+  console.log('Server listening')
+})
